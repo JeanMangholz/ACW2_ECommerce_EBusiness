d93916198f3a6856a0fbec9f49f7cec9d3591183
Jean <jean.mangholz@cpe.fr>
2018-11-26 23:21:36 +0000

    Update file names and content
    - change name of the web pages
    - update slideshow home page
    - update bibliography
    - add plan and add update menu
    -  update chatbot script
    - update media query css
    - update images

--- script/scriptChatBot.js
+++ script/scriptChatBot.js
@@ -64,6 +64,30 @@ var know={
     "Journal":"Bibliography",
     "journals":"Bibliography",
     "Journals":"Bibliography",
+    "trends":"Trends",
+    "trend":"Trends",
+    "Trends":"Trends",
+    "Trend":"Trends",
+    "Business":"UseTechnologyToBusiness",
+    "business":"UseTechnologyToBusiness",
+    "legal":"LegalSocialEthicalAspects",
+    "Legal":"LegalSocialEthicalAspects",
+    "social":"LegalSocialEthicalAspects",
+    "Social":"LegalSocialEthicalAspects",
+    "ethical":"LegalSocialEthicalAspects",
+    "Ethical":"LegalSocialEthicalAspects",
+    "ethics":"LegalSocialEthicalAspects",
+    "ethic":"LegalSocialEthicalAspects",
+    "Ethic":"LegalSocialEthicalAspects",
+    "Ethics":"LegalSocialEthicalAspects",
+    "aspect":"LegalSocialEthicalAspects",
+    "aspects":"LegalSocialEthicalAspects",
+    "Aspects":"LegalSocialEthicalAspects",
+    "Aspect":"LegalSocialEthicalAspects",
+    "description":"DescriptionTechnology",
+    "Description":"DescriptionTechnology",
+    "technology":"DescriptionTechnology",
+    "Technology":"DescriptionTechnology",
 };
 
 function talk(){
@@ -99,51 +123,51 @@ function talk(){
                 break;
 
             }
-            if(know[user].localeCompare("GeneralDescription") ==0)
+            if(know[user].localeCompare("DescriptionTechnology") ==0)
             {
                 if(page == 1)
                 {
-                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"html/generalDescription.html\">General Description</a>"+"</br></br>";
+                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"html/descriptionTechnology.html\">Description of the technology</a>"+"</br></br>";
                 }
                 else
                 {
-                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"generalDescription.html\">General Description</a>"+"</br></br>";
+                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"descriptionTechnology.html\">Description of the technology</a>"+"</br></br>";
                 }
                 break;
             }
-            if(know[user].localeCompare("KeyCriticalAreas") ==0)
+            if(know[user].localeCompare("Trends") ==0)
             {
                 if(page ==1)
                 {
-                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"html/keyCriticalAreas.html\">Key Critical Areas</a>"+"</br></br>";
+                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"html/trends.html\">Trends</a>"+"</br></br>";
                 }
                 else
                 {
-                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"keyCriticalAreas.html\">Key Critical Areas</a>"+"</br></br>";
+                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"trends.html\">Trends</a>"+"</br></br>";
                 }
                 break;
             }
-            if(know[user].localeCompare("WebTechnologyAnalysis") ==0)
+            if(know[user].localeCompare("UseTechnologyToBusiness") ==0)
             {
                 if(page==1)
                 {
-                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"html/webTechnologyAnalysis.html\">Web Technology Analysis</a>"+"</br></br>";
+                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"html/useTechnologyToBusiness.html\">Use of the technology to business</a>"+"</br></br>";
                 }
                 else
                 {
-                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"webTechnologyAnalysis.html\">Web Technology Analysis</a>"+"</br></br>";
+                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"useTechnologyToBusiness.html\">Use of the technology to business</a>"+"</br></br>";
                 }
                 break;
             }
-            if(know[user].localeCompare("EffectivenessWebsite") ==0)
+            if(know[user].localeCompare("LegalSocialEthicalAspects") ==0)
             {
                 if(page==1)
                 {
-                  document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"html/analysisEffectivinessWebsite.html\">Effectiveness of the website</a>"+"</br></br>";
+                  document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"html/legalSocialEthicalAspects.html\">Legal,social and ethical aspects</a>"+"</br></br>";
                 }
                 else
                 {
-                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"analysisEffectivinessWebsite.html\">Effectiveness of the website</a>"+"</br></br>";
+                    document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"legalSocialEthicalAspects.html\">Legal,social and ethical aspects</a>"+"</br></br>";
                 }
                 break;
             }
@@ -151,11 +175,11 @@ function talk(){
             {
                 if(page==1)
                 {
-                  document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"html/bibliography.html\">Bibliography</a>"+"</br></br>";
+                  document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"html/bibliography.html\">Bibliography</a>"+"</br></br>";
                 }
                 else
                 {
-                  document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are lookink for are located in " + know[user] + " page"+ "<a href=\"bibliography.html\">Bibliography</a>"+"</br></br>";
+                  document.getElementById("chatLog").innerHTML += "[Help Desk 2.0: ] The information that you are looking for are located in " + know[user] + " page"+ "<a href=\"bibliography.html\">Bibliography</a>"+"</br></br>";
                 }
                 break;
             }
